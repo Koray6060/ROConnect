6ba6e3447a57d8ff1350c02b77336bacfa2cc9ec
BitMap <bitmap7487@gmail.com>
2026-03-24 12:11:51 +0100

    Frontend design. :)

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
+Route::view('video-chat', 'video-chat')->name('video-chat');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
